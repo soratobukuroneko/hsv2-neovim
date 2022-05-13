94040299a45e4237907899db6f5e96da2ea5b1d6
Andrea Blankenstijn <>
2022-05-13 16:26:51 +0200

    wip update function

--- conf/nvim/lua/hsv2_utils.lua
+++ conf/nvim/lua/hsv2_utils.lua
@@ -1,7 +1,10 @@
 -- vi: et sw=4 ts=40
 -- hsv-20
 --
-local function git_repo_root(path)
+
+local M = {}
+
+function M.git_repo_root(path)
     while string.len(path) > 0 do
         if vim.fn.isdirectory(path .. '/.git') ~= 0 then
             return path
@@ -11,29 +14,39 @@ local function git_repo_root(path)
     return false
 end
 
-local function hsv2_conf_update(post_sync)
+function M.hsv2_conf_update(post_sync)
     local fterm = require('FTerm')
-    local repo = git_repo_root(os.getenv('MYVIMRC'))
+    local repo = M.git_repo_root(os.getenv('MYVIMRC'))
     if repo and vim.fn.executable('git') ~= 0 then
-        local cmd = 'cd ' .. repo
-        cmd = cmd .. '&& git stash push'
-        cmd = cmd .. '&& git checkout master'
-        cmd = cmd .. '&& git pull --rebase'
-        cmd = cmd .. '&& git checkout localconf'
-        cmd = cmd .. '&& git rebase master'
-        cmd = cmd .. '&& git stash pop'
+        local git = '!git -C ' .. repo
+        vim.api.nvim_exec(git .. ' stash push', false)
+        for _, action in ipairs({
+            'checkout master',
+            'pull --rebase',
+            'switch wip',
+            'rebase master',
+            'stash pop'
+        }) do
+            if os.getenv('?') == 0 then
+                vim.api.nvim_exec('echohl ErrorMsg', false)
+                vim.api.nvim_exec('echo ERRRRRROR', false)
+                vim.api.nvim_exec('echohl None', false)
+                break
+            end
+            vim.api.nvim_exec(git .. ' ' .. action, false)
+        end
         vim.api.nvim_exec('!' .. cmd, false)
-            vim.api.nvim_exec([[
+        vim.api.nvim_exec([[
                         0tabnew $MYVIMRC
                         Git mergetool
                         ]])
+        post_sync = next(vim.api.nvim_get_runtime_file(post_sync, false))
+        if post_sync ~= nil then
             fterm.scratch({
                 cmd = 'nvim -c "source ' .. post_sync .. '" ' .. repo .. '/History.md',
             })
+        end
     end
 end
 
-return {
-    git_repo_root = git_repo_root,
-    hsv2_conf_update = hsv2_conf_update,
-}
+return M
